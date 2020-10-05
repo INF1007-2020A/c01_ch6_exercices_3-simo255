d35e8ab88133101b2e2616b32b0dce66892e3a5c
simo255 <bmohamed255@gmail.com>
2020-10-05 17:57:06 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -3,7 +3,7 @@
 
 from random import randint
 from structs import Queue, Stack
-from collections import deqeue
+from collections import deque
 
 def reverse_data(data: list = None):
     # TODO: Demander 10 valeurs à l'utilisateur,
